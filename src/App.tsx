d06cbd929e7cef2f39d5950fbd718677df9c6690
ZxBing0066 <ZxBing0066@gmail.com>
2022-01-18 23:24:15 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Color from 'color';
 import './style.scss';
 
 import colors from './colors.json';
+import Ring from './Ring';
 
 const Context = createContext<{ handleChange: (e: MouseEvent<HTMLElement>) => void }>({ handleChange: () => {} });
 
@@ -12,30 +13,44 @@ const getFontColor = (hex: string) => {
     return color.isDark() ? color.lighten(0.5).hex() : color.darken(0.5).hex();
 };
 
-const ColorCard = ({ color, index, active }: { color: typeof colors[number]; index: number; active: boolean }) => {
-    const [bg, setBg] = useState('white');
-    const [fontColor, setFontColor] = useState('black');
-    const { name, hex } = color;
-    const { handleChange } = useContext(Context);
-    useEffect(() => {
-        setBg(hex);
-        setFontColor(getFontColor(hex));
-    }, []);
+const ColorStrip = ({ v, type }: { v: number; type: string }) => {
     return (
-        <div
-            className='color-card'
-            style={{ background: bg, color: fontColor }}
-            data-index={index}
-            data-active={active}
-            onClick={handleChange}
-        >
-            <h2>{name}</h2>
+        <div className='color-strip'>
+            <div className={'color-bar color-' + type} style={{ width: (v / 255) * 100 + '%' }}></div>
         </div>
     );
 };
 
-
-
+const ColorCard = ({ color, index, active }: { color: typeof colors[number]; index: number; active: boolean }) => {
+    const [bg, setBg] = useState('white');
+    const [fontColor, setFontColor] = useState('black');
+    const { name, hex, RGB, pinyin } = color;
+    const { handleChange } = useContext(Context);
+    useEffect(() => {
+        setBg(hex);
+        setFontColor(getFontColor(hex));
+    }, [hex]);
+    return (
+        <div
+            className='color-card'
+            style={{ color: fontColor }}
+            data-index={index}
+            data-active={active}
+            onClick={handleChange}
+        >
+            <div className='color-bar' style={{ background: bg }} />
+            <div>
+                <div className='name'>{name}</div>
+                <div>
+                    {RGB.map((v, i) => (
+                        <ColorStrip v={v} key={i} type={'rgb'[i]} />
+                    ))}
+                </div>
+                <div className='hex'>{hex}</div>
+            </div>
+        </div>
+    );
+};
 
 const ColorList = ({ currentColorIndex }: { currentColorIndex: number }) => {
     return (
@@ -48,28 +63,19 @@ const ColorList = ({ currentColorIndex }: { currentColorIndex: number }) => {
 };
 
 const Display = ({ color }: { color: typeof colors[number] }) => {
-    const { name, hex } = color;
-    const perimeter = 2 * 47 * Math.PI;
-    const strokeDasharray = [1 * perimeter, perimeter].map(v => v + 'px').join(',');
+    const { name, hex, RGB } = color;
+    const [fontColor, setFontColor] = useState('black');
+    useEffect(() => {
+        setFontColor(getFontColor(hex));
+    }, [color]);
     return (
-        <div className='display' style={{ background: hex }}>
+        <div className='display' style={{ color: fontColor }}>
             <div className='name'>{name}</div>
             <div className='hex'>{hex}</div>
-            <div>
-                <svg viewBox='0 0 100 100'>
-                    <path
-                        d='M 50,50 m 0,-47 a 47,47 0 1 1 0,94 a 47,47 0 1 1 0,-94'
-                        stroke='gray'
-                        stroke-linecap='round'
-                        stroke-width='6'
-                        opacity='1'
-                        fill-opacity='0'
-                        style={{
-                            strokeDasharray
-                        }}
-                        // style='stroke-dasharray: 221.482px, 295.31px;/* stroke-dashoffset: 0px; *//* transition: stroke-dashoffset 0s ease 0s, stroke-dasharray 0s ease 0s, stroke ease 0s, stroke-width ease 0.3s, opacity ease 0s; */'
-                    ></path>
-                </svg>
+            <div className='rgb-ring'>
+                {RGB.map((v, i) => (
+                    <Ring percent={v / 255} key={i} text={v + ''} />
+                ))}
             </div>
         </div>
     );
@@ -84,7 +90,7 @@ function App() {
     }, []);
 
     return (
-        <div className='main'>
+        <div className='main' style={{ backgroundColor: currentColor.hex }}>
             <Context.Provider value={{ handleChange }}>
                 <ColorList currentColorIndex={currentColorIndex} />
             </Context.Provider>
--- src/Ring.tsx
+++ src/Ring.tsx
@@ -1,31 +1,50 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 const Size = 100;
 const Offset = 3;
 const HalfSize = 100 / 2;
 const Radius = HalfSize - Offset;
 const Perimeter = 2 * Radius * Math.PI;
+const StrokeWidth = Offset * 2;
+const Diameter = Radius * 2;
 
 const ViewBox = `0 0 ${Size} ${Size}`;
-const Path = `M ${HalfSize},${HalfSize} m 0,-${Radius} a ${Radius},${Radius} 0 1 1 0,94 a ${Radius},${Radius} 0 1 1 0,-94`;
+const Path = `M ${HalfSize},${HalfSize} m 0,-${Radius} a ${Radius},${Radius} 0 1 1 0,${Diameter} a ${Radius},${Radius} 0 1 1 0,-${Diameter}`;
 
-const Ring = ({ percent }: { percent: number }) => {
-    const strokeDasharray = useMemo(() => `${percent * Perimeter}px, ${Perimeter}px`, [percent]);
+const BGStrokeDasharray = `${Perimeter}px, ${Perimeter}px`;
+
+const Ring = ({ percent = 0.5, text }: { percent: number; text: string }) => {
+    const [displayPercent, setDisplayPercent] = useState(0);
+    const strokeDasharray = useMemo(() => `${displayPercent * Perimeter}px, ${Perimeter}px`, [displayPercent]);
+    useEffect(() => {
+        setDisplayPercent(percent);
+    }, [percent]);
     return (
-        <svg viewBox={ViewBox}>
-            <path
-                d=''
-                stroke='gray'
-                stroke-linecap='round'
-                stroke-width='6'
-                opacity='1'
-                fill-opacity='0'
-                style={{
-                    strokeDasharray
-                }}
-                // style='stroke-dasharray: 221.482px, 295.31px;/* stroke-dashoffset: 0px; *//* transition: stroke-dashoffset 0s ease 0s, stroke-dasharray 0s ease 0s, stroke ease 0s, stroke-width ease 0.3s, opacity ease 0s; */'
-            ></path>
-        </svg>
+        <div className='ring'>
+            <div className='text'>{text}</div>
+            <svg viewBox={ViewBox} className='svg'>
+                <path
+                    d={Path}
+                    className='bg'
+                    strokeLinecap='round'
+                    strokeWidth={StrokeWidth}
+                    fillOpacity={0}
+                    style={{
+                        strokeDasharray: BGStrokeDasharray
+                    }}
+                />
+                <path
+                    d={Path}
+                    className='progress'
+                    strokeLinecap='round'
+                    strokeWidth={StrokeWidth}
+                    fillOpacity={0}
+                    style={{
+                        strokeDasharray
+                    }}
+                />
+            </svg>
+        </div>
     );
 };
 

a919c26f92fadd39c482bd80d32545c546bb918e
ZxBing0066 <ZxBing0066@gmail.com>
2022-02-01 09:47:31 +0800

    feat: ✨ add export type select

--- src/ExportModal.tsx
+++ src/ExportModal.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import hljs from 'highlight.js/lib/core';
 import scss from 'highlight.js/lib/languages/scss';
+import css from 'highlight.js/lib/languages/css';
 import 'highlight.js/styles/github.css';
 import Clipboard from 'clipboard';
 
@@ -11,6 +12,7 @@ import { getThemeColor } from './utils';
 import './export-modal.scss';
 
 hljs.registerLanguage('scss', scss);
+hljs.registerLanguage('css', css);
 
 const themeToScss = (theme: ReturnType<typeof getThemeColor>, hex: string, options: Options) => {
     return `// Generate by https://chinese-colors.heyfe.org/
@@ -24,24 +26,40 @@ $color-border-active: ${theme.lineColorActive};
 `;
 };
 
+const themeToCss = (theme: ReturnType<typeof getThemeColor>, hex: string, options: Options) => {
+    return `/* Generate by https://chinese-colors.heyfe.org/ */
+/* Primary color: ${hex}, colorAsTextColor: ${!!options.colorAsTextColor} */
+--color-text: ${theme.textColor};
+--color-text-active: ${theme.textColorActive};
+--color-background: ${theme.backgroundColor};
+--color-background-active: ${theme.backgroundColorActive};
+--color-border: ${theme.lineColor};
+--color-border-active: ${theme.lineColorActive};
+`;
+};
+
 const ExportModal = ({ color, ...modalProps }: { color: string } & Omit<ModalProps, 'children'>) => {
     const { options } = useContext(Context);
     const theme = useMemo(() => getThemeColor(color), [color]);
-    const scss = useMemo(() => themeToScss(theme, color, options), [theme]);
+    const [type, setType] = useState('scss');
     const [message, setMessage] = useState<boolean | null>(null);
-    const scssRef = useRef(scss);
+    const exportContent = useMemo(
+        () => (type === 'css' ? themeToCss : themeToScss)(theme, color, options),
+        [type, theme]
+    );
+    const exportContentRef = useRef(exportContent);
 
     const codeRef = useRef(null);
     const copyButtonRef = useRef<HTMLButtonElement>(null);
 
     useEffect(() => {
         codeRef.current && hljs.highlightElement(codeRef.current);
-        scssRef.current = scss;
-    }, [scss]);
+        exportContentRef.current = exportContent;
+    }, [exportContent]);
 
     useEffect(() => {
         const clipboard = new Clipboard(copyButtonRef.current!, {
-            text: () => scssRef.current
+            text: () => exportContentRef.current
         });
 
         clipboard.on('success', function () {
@@ -74,19 +92,27 @@ const ExportModal = ({ color, ...modalProps }: { color: string } & Omit<ModalPro
             header="导出主题变量"
             footer={
                 <div className="footer">
-                    {message === null ? null : (
-                        <span className="copy-tip">{message ? '复制成功' : '复制失败请手动复制'}</span>
-                    )}
-                    <button ref={copyButtonRef} className="copy-button">
-                        复制
-                    </button>
+                    <div>
+                        <select value={type} onChange={e => setType(e.target.value)} className="export-type">
+                            <option value="scss">SCSS</option>
+                            <option value="css">CSS</option>
+                        </select>
+                    </div>
+                    <div>
+                        {message === null ? null : (
+                            <span className="copy-tip">{message ? '复制成功' : '复制失败请手动复制'}</span>
+                        )}
+                        <button ref={copyButtonRef} className="copy-button">
+                            复制
+                        </button>
+                    </div>
                 </div>
             }
         >
             <div className="export-content">
                 <pre>
-                    <code className="language-scss" ref={codeRef}>
-                        {scss}
+                    <code className={'language-' + type} ref={codeRef}>
+                        {exportContent}
                     </code>
                 </pre>
             </div>

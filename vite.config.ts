594cc6b38c1593b01b45fafc9897e43fca8235ef
ZxBing0066 <ZxBing0066@gmail.com>
2022-02-08 19:40:24 +0800

    feat: ✨ add analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import visualizer from 'rollup-plugin-visualizer';
+
+const plugins: any[] = [react()];
+
+// if (process.env.NODE_ENV === 'production') {
+//     // 打包依赖展示
+//     plugins.push(
+//         visualizer({
+//             open: true,
+//             gzipSize: true,
+//             brotliSize: true
+//         })
+//     );
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins,
     resolve: {
         dedupe: ['react', 'react-dom']
     }
